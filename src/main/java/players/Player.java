90f781477b2061c4f47b7604ac79180a51fd6b1c
Kyle Lin <kylelin47@gmail.com>
2015-08-04 18:46:10 -0400

    Switch from Netbeans to Eclipse

--- src/main/java/players/Player.java
+++ src/main/java/players/Player.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package my.basketballsim;
+package players;
 
 import java.util.ArrayList;
 
--- src/main/java/simulator/Simulator.java
+++ src/main/java/simulator/Simulator.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package my.basketballsim;
+package simulator;
 
 import static java.lang.Math.random;
+import players.Player;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
